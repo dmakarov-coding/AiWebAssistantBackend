82dc080c54adfa12fc08c61e318462d8354ddef8
dmakarov <dmakarov.dev@gmail.com>
2024-06-01 11:05:05 -0400

    update

--- services/gpt-service.js
+++ services/gpt-service.js
@@ -1,21 +1,36 @@
 //This module is used to add color to the console output, making it easier to read debug and log messages.
 require('colors');
+//This imports the EventEmitter class from Node.js, which allows the GptService class to emit
+// and listen for events.
 const EventEmitter = require('events');
+//This imports the OpenAI class, which will be used to interact with the OpenAI API.
 const OpenAI = require('openai');
-//test
 
 class GptService extends EventEmitter {
+  //This is the constructor method for the GptService class, which initializes an instance of the class.
   constructor(businessDescription) {
     super();
+    //Create an instance of the OpenAI class and assign it to the openai property of the GptService instance.
     this.openai = new OpenAI();
+    //Initializes the userContext property, which is an array containing a single object.
+    //This object defines the role and initial context for the conversation with the OpenAI API.
+    //The context specifies that appointment bookings are not provided and includes the provided businessDescription.
+    //This is important for setting the initial behavior and scope of the AI's responses.
     this.userContext = [
-      { 'role': 'system', 'content': `IMPORTANT: You dont provide appointment bookings.
-      ${businessDescription}` }
+      { 'role': 'system', 'content': `${businessDescription}` }
     ];
+
     this.partialResponseIndex = 0;
   }
 
   setCallSid(callSid) {
+    //This line adds a new object to the userContext array. The object has two properties:
+    //'role': 'system': This specifies that the role of this message is system,
+    //indicating that it is a system-level message and not from a user or assistant.
+    //'content': callSid: ${callSid}``: This sets the content of the message to include the callSid.
+    //By embedding the callSid in the context, it ensures that this identifier is included
+    //in the context for subsequent interactions with the OpenAI API, which
+    //is useful for tracking the conversation or for any logic dependent on the callSid.
     this.userContext.push({ 'role': 'system', 'content': `callSid: ${callSid}` });
   }
 
